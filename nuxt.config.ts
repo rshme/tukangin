cffae346cf6d2e76fb370032831fcde7d348d021
Rafi Septian Hadi <rafiseptian90@gmail.com>
2025-08-16 15:56:44 +0700

    feat: Add vueuse and pinia package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-feather-icons']
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'nuxt-feather-icons',
+    '@vueuse/nuxt',
+    '@pinia/nuxt'
+  ]
 })
